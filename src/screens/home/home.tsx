90e4ccd076280a59d762518d9f3925202cb82931
reptiliano37 <miguel.arara5@gmail.com>
2022-03-21 19:10:07 +0100

    Successfully connections to the api creating artits and albums

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { View, Text, Alert, TouchableOpacity,Button,Image, ScrollViewBase, ScrollView, SafeAreaView } from 'react-native';
 import styles from './home.styles'
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
@@ -6,13 +6,36 @@ import {StackNavigatorParams} from '../../config/navigator'
 import { LinearGradient } from 'expo-linear-gradient';
 import AddButton from '../../components/addButton/addButton'
 import { Title,Divider } from 'react-native-paper';
+import FadeInOut from 'react-native-fade-in-out';
+import mongoose, { Schema } from 'mongoose';
 
 type HomeProps = {
     navigation: NativeStackNavigationProp<StackNavigatorParams, "Home">
   }
+let infoAlbum =
+  {
+    "title": "string",
+    "artistId": "string",
+    "coverUrl": "string",
+    "year": 0,
+    "genre": "string"
+  }
   
   export default function Home(): ReactElement {
-    
+
+  const [visible, setVisible] = useState(true);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const toggleVisible = () => {
+    setVisible(!visible);
+  }
+  const toggleModalVisible = () => {
+    setModalVisible(!modalVisible);
+  }
+  const ModalAlbum = () =>{
+
+  }
+
   const ItemDivider = () => {
     return (
       <View
@@ -37,30 +60,86 @@ type HomeProps = {
     }
     const HelpMessage = () =>{
       return(
-        <View style={{backgroundColor:'rgba(0,0,0,0.2)',
-                      flexDirection: "row", justifyContent:"center",alignItems:"center", borderWidth:1, borderColor:'white', width: 300,
-                      height: 100,
-                      bottom:20,
-                      flex:1,
-                      borderRadius: 20,}}>
-          <Text style={{color:'black',fontSize:20}}>Ups, there are no albums or artists added in the app! </Text>
-        </View>
+        <FadeInOut visible={visible} rotate={true} scale={true} duration={3000}>
+          <View style={{backgroundColor:'rgba(0,0,0,0.2)',
+                        flexDirection: "row", justifyContent:"center",alignItems:"center", borderWidth:1, borderColor:'white', width: 300,
+                        height: 100,
+                        bottom:20,
+                        borderRadius: 20,}}>
+            <Text style={{color:'black',fontSize:20}}>Ups, there are no albums or artists added in the app! </Text>
+          </View>
+        </FadeInOut>
       )
     }
-    const Albums = () =>{
+    
+
+    const createAlbum = async () => {
+      const urlAlbum = "http://192.168.1.46:3000/album"
+      try {
+        const response = await fetch(
+          urlAlbum,{
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title: "Los Carmenes",
+          artistId: "6238bdebc38eb300132e0043",
+          coverUrl: "www.url.com",
+          year: 1936,
+          genre: "pop" })
+          
+      })
+      const json = await response.json();
+      console.log(json)
+      } catch (error) {
+          console.error(error);
+        }
+    }
+    const createArtist = async () => {
+      const urlArtist = "http://192.168.1.46:3000/artist"
+      try {
+        const response = await fetch(
+          urlArtist,{
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: "Jose Luis Perales",
+          photoUrl: "string",
+          birthdate: "1939-05-18",
+          deathDate: "2021-05-18" })
+      })
+      const json = await response.json();
+      console.log(json)
+      } catch (error) {
+          console.error(error);
+        }
+    }
+
+     const Albums = () =>{
       return(
         <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
           <Text style={{color:'black',fontStyle:'italic', fontWeight:'bold',fontSize:20, margin:5}}> Create an Album</Text>
-          <AddButton title='+' style={styles.addButtonStyle}></AddButton>
+          <AddButton title='+' style={styles.addButtonStyle}  onPress={()=>{
+              createAlbum();
+            }}></AddButton>
         </View>
       )
     }
     const Artists = () =>{
       return(
-        <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center',margin:20}}>
-          <Text style={{color:'black',fontStyle:'italic', fontWeight:'bold',fontSize:20, margin:5}}> Create an Artist</Text>
-          <AddButton title='+' style={styles.addButtonStyle}></AddButton>
-        </View>
+        
+          <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center',margin:20}}>
+            <Text style={{color:'black',fontStyle:'italic', fontWeight:'bold',fontSize:20, margin:5}}> Create an Artist</Text>
+            <AddButton title='+' style={styles.addButtonStyle} onPress={()=>{
+              createArtist()
+            }}></AddButton>
+          </View>
+        
       )
     }
 
@@ -68,7 +147,9 @@ type HomeProps = {
       <LinearGradient locations={[0, 0.3]} colors={['white','rgb(193, 244, 228)']} style={{flex:1}}>
         <View  style={styles.container}>
           <Header/>
-          <HelpMessage/>
+          
+            <HelpMessage/>
+          
           <SafeAreaView style={{flex:3}}>
           <ScrollView style={{flexGrow:1}}>
             <Albums/>
